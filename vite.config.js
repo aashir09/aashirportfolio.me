8d83409983e9bc70809109d11e2785b2830cdb4a
aashir09 <aashirmansuri@gmail.com>
2023-10-20 15:58:59 +0530

    version1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
 
-  base:'https://github.com/aashir09/aashirportfolio.me',
-  plugins: [react()],
+   plugins: [react()],
 })
